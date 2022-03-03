eb5cc8eb0b882bd22745f10a306b179ee0445b7f
deanls <deanqs@qq.com>
2022-03-03 10:37:42 +0800

    fix:iAMStoreAPI interface

--- http/objectstore/iam/iam-level-store.go
+++ http/objectstore/iam/iam-level-store.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"context"
+	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
 	"github.com/filedag-project/filedag-storage/http/objectstore/uleveldb"
 )
 
@@ -10,22 +11,22 @@ type IAMLevelDBStore struct {
 	db *uleveldb.Uleveldb
 }
 
-func (I *IAMLevelDBStore) loadUser(ctx context.Context, user string) error {
+func (I *IAMLevelDBStore) loadUser(ctx context.Context, user string, m map[string]auth.Credentials) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadUsers(ctx context.Context) error {
+func (I *IAMLevelDBStore) loadUsers(ctx context.Context, m map[string]auth.Credentials) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadGroup(ctx context.Context, group string) error {
+func (I *IAMLevelDBStore) loadGroup(ctx context.Context, group string, m map[string]GroupInfo) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadGroups(ctx context.Context) error {
+func (I *IAMLevelDBStore) loadGroups(ctx context.Context, m map[string]GroupInfo) error {
 	//TODO implement me
 	panic("implement me")
 }
--- http/objectstore/iam/iam-store.go
+++ http/objectstore/iam/iam-store.go
@@ -2,14 +2,15 @@ package iam
 
 import (
 	"context"
+	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
 )
 
 // IAMStoreAPI defines an interface for the IAM persistence layer
 type IAMStoreAPI interface {
-	loadUser(ctx context.Context, user string) error
-	loadUsers(ctx context.Context) error
-	loadGroup(ctx context.Context, group string) error
-	loadGroups(ctx context.Context) error
+	loadUser(ctx context.Context, user string, m map[string]auth.Credentials) error
+	loadUsers(ctx context.Context, m map[string]auth.Credentials) error
+	loadGroup(ctx context.Context, group string, m map[string]GroupInfo) error
+	loadGroups(ctx context.Context, m map[string]GroupInfo) error
 	saveUserIdentity(ctx context.Context, name string, u UserIdentity) error
 	saveGroupInfo(ctx context.Context, group string, gi GroupInfo) error
 	deleteUserIdentity(ctx context.Context, name string) error
