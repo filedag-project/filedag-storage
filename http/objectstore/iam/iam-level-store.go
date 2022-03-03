9048bc86c9e8232b751cce353cec41fd52c42fec
deanls <deanqs@qq.com>
2022-03-03 10:32:34 +0800

    fix:iAMStoreAPI interface

--- http/objectstore/iam/iam-level-store.go
+++ http/objectstore/iam/iam-level-store.go
@@ -2,7 +2,6 @@ package iam
 
 import (
 	"context"
-	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
 	"github.com/filedag-project/filedag-storage/http/objectstore/uleveldb"
 )
 
@@ -11,47 +10,22 @@ type IAMLevelDBStore struct {
 	db *uleveldb.Uleveldb
 }
 
-func (I *IAMLevelDBStore) loadPolicyDoc(ctx context.Context, policy string, m map[string]PolicyDoc) error {
+func (I *IAMLevelDBStore) loadUser(ctx context.Context, user string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadPolicyDocs(ctx context.Context, m map[string]PolicyDoc) error {
+func (I *IAMLevelDBStore) loadUsers(ctx context.Context) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadUser(ctx context.Context, user string, m map[string]auth.Credentials) error {
+func (I *IAMLevelDBStore) loadGroup(ctx context.Context, group string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (I *IAMLevelDBStore) loadUsers(ctx context.Context, m map[string]auth.Credentials) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) loadGroup(ctx context.Context, group string, m map[string]GroupInfo) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) loadGroups(ctx context.Context, m map[string]GroupInfo) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) loadMappedPolicy(ctx context.Context, name string, isGroup bool, m map[string]MappedPolicy) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) loadMappedPolicies(ctx context.Context, isGroup bool, m map[string]MappedPolicy) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) savePolicyDoc(ctx context.Context, policyName string, p PolicyDoc) error {
+func (I *IAMLevelDBStore) loadGroups(ctx context.Context) error {
 	//TODO implement me
 	panic("implement me")
 }
@@ -62,16 +36,6 @@ func (I *IAMLevelDBStore) saveUserIdentity(ctx context.Context, name string, u U
 }
 
 func (I *IAMLevelDBStore) saveGroupInfo(ctx context.Context, group string, gi GroupInfo) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) deletePolicyDoc(ctx context.Context, policyName string) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (I *IAMLevelDBStore) deleteMappedPolicy(ctx context.Context, name string, isGroup bool) error {
 	//TODO implement me
 	panic("implement me")
 }
--- http/objectstore/iam/iam-store.go
+++ http/objectstore/iam/iam-store.go
@@ -2,24 +2,16 @@ package iam
 
 import (
 	"context"
-	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
 )
 
 // IAMStoreAPI defines an interface for the IAM persistence layer
 type IAMStoreAPI interface {
-	loadPolicyDoc(ctx context.Context, policy string, m map[string]PolicyDoc) error
-	loadPolicyDocs(ctx context.Context, m map[string]PolicyDoc) error
-	loadUser(ctx context.Context, user string, m map[string]auth.Credentials) error
-	loadUsers(ctx context.Context, m map[string]auth.Credentials) error
-	loadGroup(ctx context.Context, group string, m map[string]GroupInfo) error
-	loadGroups(ctx context.Context, m map[string]GroupInfo) error
-	loadMappedPolicy(ctx context.Context, name string, isGroup bool, m map[string]MappedPolicy) error
-	loadMappedPolicies(ctx context.Context, isGroup bool, m map[string]MappedPolicy) error
-	savePolicyDoc(ctx context.Context, policyName string, p PolicyDoc) error
+	loadUser(ctx context.Context, user string) error
+	loadUsers(ctx context.Context) error
+	loadGroup(ctx context.Context, group string) error
+	loadGroups(ctx context.Context) error
 	saveUserIdentity(ctx context.Context, name string, u UserIdentity) error
 	saveGroupInfo(ctx context.Context, group string, gi GroupInfo) error
-	deletePolicyDoc(ctx context.Context, policyName string) error
-	deleteMappedPolicy(ctx context.Context, name string, isGroup bool) error
 	deleteUserIdentity(ctx context.Context, name string) error
 	deleteGroupInfo(ctx context.Context, name string) error
 }
