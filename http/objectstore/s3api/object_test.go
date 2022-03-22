afa0136349da270467a5747fac0d2ba8ab70fb3c
deanls <deanqs@qq.com>
2022-03-22 09:07:38 +0800

    test:test GetObjectHandler

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -26,3 +26,21 @@ func TestS3ApiServer_PutObjectHandler(t *testing.T) {
 	fmt.Println(string(body))
 
 }
+func TestS3ApiServer_GetObjectHandler(t *testing.T) {
+	u := "http://127.0.0.1:9985/test/1.txt"
+	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", "test", "test", t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
