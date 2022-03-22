1f59695a3a7d76202da8cb448fd589f31426fbc5
deanls <deanqs@qq.com>
2022-03-22 15:57:06 +0800

    test:test copy object,and fix get object

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/filedag-project/filedag-storage/http/objectstore/utils/testsign"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -29,6 +30,42 @@ func TestS3ApiServer_PutObjectHandler(t *testing.T) {
 func TestS3ApiServer_GetObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test/1.txt"
 	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", "test", "test", t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
+func TestS3ApiServer_CopyObjectHandler(t *testing.T) {
+	u := "http://127.0.0.1:9985/test1/11.txt"
+	req := testsign.MustNewSignedV4Request(http.MethodPut, u, 0, nil, "s3", "test", "test", t)
+	req.Header.Set("X-Amz-Copy-Source", url.QueryEscape("/test/1.txt"))
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
+func TestS3ApiServer_HeadObjectHandler(t *testing.T) {
+	u := "http://127.0.0.1:9985/test/1.txt"
+	req := testsign.MustNewSignedV4Request(http.MethodHead, u, 0, nil, "s3", "test", "test", t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
