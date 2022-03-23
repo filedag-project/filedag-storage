195f3f748566eb4e991bf4d45cdcd86f979dc14b
deanls <deanqs@qq.com>
2022-03-23 16:15:56 +0800

    fix:fix test sts

--- http/objectstore/s3api/sts_test.go
+++ http/objectstore/s3api/sts_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestStsAPIHandlers_AssumeRole(t *testing.T) {
 	body := bytes.NewReader([]byte("Version=2011-06-15&Action=AssumeRole"))
-	req := testsign.MustNewSignedV4Request(http.MethodPost, "http://127.0.0.1:9985/", 0, body, "sts", "test", "test", t)
+	req := testsign.MustNewSignedV4Request(http.MethodPost, "http://127.0.0.1:9985/", 0, body, "sts", "test1", "testsecretKey", t)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	fmt.Println(err)
