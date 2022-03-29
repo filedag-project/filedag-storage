03e78259be27a9790c45e5355eba8231dbba0d4f
deanls <deanqs@qq.com>
2022-03-29 11:49:31 +0800

    test:test new policy use user "test"

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -11,15 +11,15 @@ import (
 )
 
 const (
-	accessKeyTmp = "BOLPNGFEVIWN4SV36YEE"
-	secretKeyTmp = "8KPlsjxXdrlPH+P2VXIDWHv61YsOGkP++Tp+oC7j"
+	DefaultTestAccessKey = "test"
+	DefaultTestSecretKey = "test"
 )
 
 func TestS3ApiServer_PutObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test/1.txt"
 	r1, _ := ioutil.ReadFile("./object_test.go")
 
-	req := testsign.MustNewSignedV4Request(http.MethodPut, u, int64(len(r1)), bytes.NewReader(r1), "s3", "test", "test", t)
+	req := testsign.MustNewSignedV4Request(http.MethodPut, u, int64(len(r1)), bytes.NewReader(r1), "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
@@ -37,7 +37,7 @@ func TestS3ApiServer_PutObjectHandler(t *testing.T) {
 }
 func TestS3ApiServer_GetObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test/1.txt"
-	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", accessKeyTmp, secretKeyTmp, t)
+	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
@@ -55,7 +55,7 @@ func TestS3ApiServer_GetObjectHandler(t *testing.T) {
 }
 func TestS3ApiServer_CopyObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test1/11.txt"
-	req := testsign.MustNewSignedV4Request(http.MethodPut, u, 0, nil, "s3", accessKeyTmp, secretKeyTmp, t)
+	req := testsign.MustNewSignedV4Request(http.MethodPut, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 	req.Header.Set("X-Amz-Copy-Source", url.QueryEscape("/test/1.txt"))
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
@@ -73,7 +73,7 @@ func TestS3ApiServer_CopyObjectHandler(t *testing.T) {
 }
 func TestS3ApiServer_HeadObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test/1.txt"
-	req := testsign.MustNewSignedV4Request(http.MethodHead, u, 0, nil, "s3", accessKeyTmp, secretKeyTmp, t)
+	req := testsign.MustNewSignedV4Request(http.MethodHead, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
