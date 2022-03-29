367fc058e4798bf5dd643c979d8234340f4c10a3
deanls <deanqs@qq.com>
2022-03-29 17:37:44 +0800

    test:test list object

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -16,7 +16,7 @@ const (
 )
 
 func TestS3ApiServer_PutObjectHandler(t *testing.T) {
-	u := "http://127.0.0.1:9985/test/1.txt"
+	u := "http://127.0.0.1:9985/test22/1.txt"
 	r1, _ := ioutil.ReadFile("./object_test.go")
 
 	req := testsign.MustNewSignedV4Request(http.MethodPut, u, int64(len(r1)), bytes.NewReader(r1), "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
@@ -74,6 +74,24 @@ func TestS3ApiServer_CopyObjectHandler(t *testing.T) {
 func TestS3ApiServer_HeadObjectHandler(t *testing.T) {
 	u := "http://127.0.0.1:9985/test/1.txt"
 	req := testsign.MustNewSignedV4Request(http.MethodHead, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
+func TestS3ApiServer_ListObjectsV1Handler(t *testing.T) {
+	u := "http://127.0.0.1:9985/test22"
+	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
