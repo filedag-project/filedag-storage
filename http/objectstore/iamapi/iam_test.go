66492957b00dfadb82ada2b76f9b248038d22842
deanls <deanqs@qq.com>
2022-03-31 17:01:41 +0800

    test:test RemoveUser PutUserPolicy GetUserPolicy RemoveUserPolicy

--- http/objectstore/iamapi/iam_test.go
+++ http/objectstore/iamapi/iam_test.go
@@ -32,11 +32,87 @@ func TestIamApiServer_GetUserList(t *testing.T) {
 	fmt.Println(string(body))
 }
 func TestIamApiServer_AddUser(t *testing.T) {
-	urlValues := make(url.Values)
-	urlValues.Set("accessKey", "test2")
-	urlValues.Set("secretKey", "test12345")
-	u := "http://127.0.0.1:9985/admin/v1/add-user"
+	u := "http://127.0.0.1:9985/admin/v1/add-user?accessKey=test1&secretKey=test12345"
 	req := testsign.MustNewSignedV4Request(http.MethodPost, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
+func TestIamApiServer_RemoveUser(t *testing.T) {
+	u := "http://127.0.0.1:9985/admin/v1/remove-user?accessKey=test1"
+	req := testsign.MustNewSignedV4Request(http.MethodPost, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
+func TestIamApiServer_PutUserPolicy(t *testing.T) {
+	urlValues := make(url.Values)
+	policy := `{"Version":"2008-10-17","Statement":[{"Effect":"Allow","Sid":"1","Principal":{"AWS":["111122223333","444455556666"]},"Action":["s3:*"],"Resource":"arn:aws:s3:::test22/*"}]}`
+	urlValues.Set("policyDocument", policy)
+	urlValues.Set("userName", "test1")
+	urlValues.Set("policyName", "read")
+	u := "http://127.0.0.1:9985/admin/v1/put-user-policy?"
+	req := testsign.MustNewSignedV4Request(http.MethodPost, u+urlValues.Encode(), 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
+func TestIamApiServer_GetUserPolicy(t *testing.T) {
+	urlValues := make(url.Values)
+	urlValues.Set("userName", "test1")
+	urlValues.Set("policyName", "read")
+	u := "http://127.0.0.1:9985/admin/v1/get-user-policy?"
+	req := testsign.MustNewSignedV4Request(http.MethodGet, u+urlValues.Encode(), 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
+func TestIamApiServer_RemoveUserPolicy(t *testing.T) {
+	urlValues := make(url.Values)
+	urlValues.Set("userName", "test1")
+	urlValues.Set("policyName", "read")
+	u := "http://127.0.0.1:9985/admin/v1/remove-user-policy?"
+	req := testsign.MustNewSignedV4Request(http.MethodGet, u+urlValues.Encode(), 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
 
 	//req.Header.Set("Content-Type", "text/plain")
 	client := &http.Client{}
