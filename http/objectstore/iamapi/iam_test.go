04fd875f62df254d068dc4df794f3e6c165bc743
deanls <deanqs@qq.com>
2022-04-06 11:56:17 +0800

    test:test ChangePassword

--- http/objectstore/iamapi/iam_test.go
+++ http/objectstore/iamapi/iam_test.go
@@ -65,6 +65,27 @@ func TestIamApiServer_RemoveUser(t *testing.T) {
 	fmt.Println(res)
 	fmt.Println(string(body))
 }
+
+func TestIamApiServer_ChangePassword(t *testing.T) {
+	urlValues := make(url.Values)
+	urlValues.Set("newPassword", "test2222")
+	u := "http://127.0.0.1:9985/admin/v1/change-password?"
+	req := testsign.MustNewSignedV4Request(http.MethodPost, u+urlValues.Encode(), 0, nil, "s3", "test1", "test12345", t)
+
+	//req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
+
 func TestIamApiServer_PutUserPolicy(t *testing.T) {
 	urlValues := make(url.Values)
 	policy := `{"Version":"2008-10-17","Statement":[{"Effect":"Allow","Sid":"1","Principal":{"AWS":["111122223333","444455556666"]},"Action":["s3:*"],"Resource":"arn:aws:s3:::test22/*"}]}`
