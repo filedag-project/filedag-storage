7bec7065e27f166443d3513f121d0583cd067a67
deanls <deanqs@qq.com>
2022-04-08 16:07:44 +0800

    fix:duplicate populate payload

--- http/objectstore/iam/check_handler_auth.go
+++ http/objectstore/iam/check_handler_auth.go
@@ -72,9 +72,6 @@ func (s *AuthSys) CheckRequestAuthTypeCredential(ctx context.Context, r *http.Re
 
 		// Populate payload to extract location constraint.
 		r.Body = ioutil.NopCloser(bytes.NewReader(payload))
-
-		// Populate payload again to handle it in HTTP handler.
-		r.Body = ioutil.NopCloser(bytes.NewReader(payload))
 		pol, err := s.PolicySys.bmSys.GetMeta(bucketName, cred.AccessKey)
 		if pol != (BucketMetadata{}) {
 			return cred, owner, api_errors.ErrBucketAlreadyExists
