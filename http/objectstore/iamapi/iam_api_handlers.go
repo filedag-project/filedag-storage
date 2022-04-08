4f6f49ac5369bfd8cf9ecfb6f34d0ace26f36cf0
deanls <deanqs@qq.com>
2022-04-08 15:59:18 +0800

    perf: make the IAM API interface simple and meet user needs at the same time

--- http/objectstore/iamapi/iam_api_handlers.go
+++ http/objectstore/iamapi/iam_api_handlers.go
@@ -2,9 +2,7 @@ package iamapi
 
 import (
 	"context"
-	"crypto/sha1"
 	"encoding/json"
-	"fmt"
 	"github.com/filedag-project/filedag-storage/http/objectstore/api_errors"
 	"github.com/filedag-project/filedag-storage/http/objectstore/iam"
 	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
@@ -12,29 +10,13 @@ import (
 	"github.com/filedag-project/filedag-storage/http/objectstore/response"
 	"github.com/gorilla/mux"
 	"net/http"
-	"sync"
 )
 
 const (
 	policyDocumentVersion = "2012-10-17"
 )
 
-var policyLock = sync.RWMutex{}
-
-//GetUserList get all user
-func (iamApi *iamApiServer) GetUserList(w http.ResponseWriter, r *http.Request) {
-	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
-	if s3err != api_errors.ErrNone {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
-		return
-	}
-	var resp ListUsersResponse
-	resp.ListUsersResult.Users = iamApi.authSys.Iam.GetUserList(context.Background())
-	response.WriteXMLResponse(w, r, http.StatusOK, resp)
-}
-
 // CreateUser  add user
-//https://docs.aws.amazon.com/IAM/latest/APIReference/API_CreateUser.html
 func (iamApi *iamApiServer) CreateUser(w http.ResponseWriter, r *http.Request) {
 	_, ok, _ := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
 	if !ok {
@@ -55,7 +37,6 @@ func (iamApi *iamApiServer) CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 //DeleteUser delete user
-//https://docs.aws.amazon.com/IAM/latest/APIReference/API_DeleteUser.html
 func (iamApi *iamApiServer) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
 	if s3err != api_errors.ErrNone {
@@ -69,6 +50,104 @@ func (iamApi *iamApiServer) DeleteUser(w http.ResponseWriter, r *http.Request) {
 		response.WriteErrorResponse(w, r, api_errors.ErrInternalError)
 		return
 	}
+	response.WriteXMLResponse(w, r, http.StatusOK, resp)
+}
+
+//GetUserInfo get user info
+func (iamApi *iamApiServer) GetUserInfo(w http.ResponseWriter, r *http.Request) {
+	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
+	if s3err != api_errors.ErrNone {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
+		return
+	}
+	userName := r.FormValue("userName")
+	ctx := context.Background()
+	cred, ok := iamApi.authSys.Iam.GetUserInfo(ctx, userName)
+	if !ok {
+		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrAccessKeyDisabled), r.URL, r.Host)
+		return
+	}
+	polices, err := iamApi.authSys.Iam.GetUserPolices(ctx, userName)
+	if err != nil {
+		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrInternalError), r.URL, r.Host)
+		return
+	}
+
+	user := iam.UserInfo{
+		SecretKey:  cred.SecretKey,
+		PolicyName: polices,
+		Status:     iam.AccountStatus(cred.Status),
+	}
+
+	data, err := json.Marshal(user)
+	if err != nil {
+		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrJsonMarshal), r.URL, r.Host)
+		return
+	}
+	response.WriteSuccessResponseJSON(w, data)
+}
+
+// ChangePassword change password
+func (iamApi *iamApiServer) ChangePassword(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	cred, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(ctx, r, "", "", "")
+	if s3err != api_errors.ErrNone {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
+		return
+	}
+
+	secret := r.FormValue("newPassword")
+	if !auth.IsSecretKeyValid(secret) {
+		response.WriteErrorResponse(w, r, api_errors.ErrInvalidQueryParams)
+	}
+	c, ok := iamApi.authSys.Iam.GetUser(ctx, cred.AccessKey)
+	if !ok {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessKeyDisabled)
+		return
+	}
+	c.SecretKey = secret
+	err := iamApi.authSys.Iam.UpdateUser(ctx, c)
+	if err != nil {
+		response.WriteErrorResponse(w, r, api_errors.ErrInternalError)
+		return
+	}
+	response.WriteSuccessResponseEmpty(w, r)
+}
+
+// SetStatus set user status
+func (iamApi *iamApiServer) SetStatus(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(ctx, r, "", "", "")
+	if s3err != api_errors.ErrNone {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
+		return
+	}
+
+	user := r.FormValue("userName")
+	status := r.FormValue("status")
+	c, ok := iamApi.authSys.Iam.GetUser(ctx, user)
+	if !ok {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessKeyDisabled)
+		return
+	}
+	c.Status = status
+	err := iamApi.authSys.Iam.UpdateUser(ctx, c)
+	if err != nil {
+		response.WriteErrorResponse(w, r, api_errors.ErrInternalError)
+		return
+	}
+	response.WriteSuccessResponseEmpty(w, r)
+}
+
+//GetUserList get all user
+func (iamApi *iamApiServer) GetUserList(w http.ResponseWriter, r *http.Request) {
+	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
+	if s3err != api_errors.ErrNone {
+		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
+		return
+	}
+	var resp ListUsersResponse
+	resp.ListUsersResult.Users = iamApi.authSys.Iam.GetUserList(context.Background())
 	response.WriteXMLResponse(w, r, http.StatusOK, resp)
 }
 
@@ -174,13 +253,8 @@ func GetPolicyDocument(policyD *string) (policyDocument policy.PolicyDocument, e
 	}
 	return policyDocument, err
 }
-func Hash(s *string) string {
-	h := sha1.New()
-	h.Write([]byte(*s))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
 
-//CreatePolicy Create Policy
+/*//CreatePolicy Create Policy
 func (iamApi *iamApiServer) CreatePolicy(w http.ResponseWriter, r *http.Request) {
 	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
 	if s3err != api_errors.ErrNone {
@@ -207,86 +281,4 @@ func (iamApi *iamApiServer) CreatePolicy(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	response.WriteXMLResponse(w, r, http.StatusOK, resp)
-}
-
-//GetUserInfo get user info
-func (iamApi *iamApiServer) GetUserInfo(w http.ResponseWriter, r *http.Request) {
-	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(context.Background(), r, "", "", "")
-	if s3err != api_errors.ErrNone {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
-		return
-	}
-	userName := r.FormValue("userName")
-	ctx := context.Background()
-	cred, ok := iamApi.authSys.Iam.GetUserInfo(ctx, userName)
-	if !ok {
-		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrAccessKeyDisabled), r.URL, r.Host)
-		return
-	}
-	polices, err := iamApi.authSys.Iam.GetUserPolices(ctx, userName)
-	if err != nil {
-		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrInternalError), r.URL, r.Host)
-		return
-	}
-
-	user := iam.UserInfo{
-		SecretKey:  cred.SecretKey,
-		PolicyName: polices,
-		Status:     iam.AccountStatus(cred.Status),
-	}
-
-	data, err := json.Marshal(user)
-	if err != nil {
-		response.WriteErrorResponseJSON(ctx, w, api_errors.GetAPIError(api_errors.ErrJsonMarshal), r.URL, r.Host)
-		return
-	}
-	response.WriteSuccessResponseJSON(w, data)
-}
-func (iamApi *iamApiServer) ChangePassword(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	cred, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(ctx, r, "", "", "")
-	if s3err != api_errors.ErrNone {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
-		return
-	}
-
-	secret := r.FormValue("newPassword")
-	if !auth.IsSecretKeyValid(secret) {
-		response.WriteErrorResponse(w, r, api_errors.ErrInvalidQueryParams)
-	}
-	c, ok := iamApi.authSys.Iam.GetUser(ctx, cred.AccessKey)
-	if !ok {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessKeyDisabled)
-		return
-	}
-	c.SecretKey = secret
-	err := iamApi.authSys.Iam.UpdateUser(ctx, c)
-	if err != nil {
-		response.WriteErrorResponse(w, r, api_errors.ErrInternalError)
-		return
-	}
-	response.WriteSuccessResponseEmpty(w, r)
-}
-func (iamApi *iamApiServer) SetStatus(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	_, _, s3err := iamApi.authSys.CheckRequestAuthTypeCredential(ctx, r, "", "", "")
-	if s3err != api_errors.ErrNone {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessDenied)
-		return
-	}
-
-	user := r.FormValue("userName")
-	status := r.FormValue("status")
-	c, ok := iamApi.authSys.Iam.GetUser(ctx, user)
-	if !ok {
-		response.WriteErrorResponse(w, r, api_errors.ErrAccessKeyDisabled)
-		return
-	}
-	c.Status = status
-	err := iamApi.authSys.Iam.UpdateUser(ctx, c)
-	if err != nil {
-		response.WriteErrorResponse(w, r, api_errors.ErrInternalError)
-		return
-	}
-	response.WriteSuccessResponseEmpty(w, r)
-}
+}*/
--- http/objectstore/iamapi/iam_api_server.go
+++ http/objectstore/iamapi/iam_api_server.go
@@ -23,23 +23,26 @@ func NewIamApiServer(router *mux.Router) {
 func (iamApi *iamApiServer) registerRouter(router *mux.Router) {
 	// API Router
 	apiRouter := router.PathPrefix("/admin/v1").Subrouter()
-	apiRouter.Methods(http.MethodGet).Path("/list-user").HandlerFunc(iamApi.GetUserList)
+	//root user
 	apiRouter.Methods(http.MethodPost).Path("/add-user").HandlerFunc(iamApi.CreateUser).Queries("accessKey", "{accessKey:.*}", "secretKey", "{secretKey:.*}")
 	apiRouter.Methods(http.MethodPost).Path("/remove-user").HandlerFunc(iamApi.DeleteUser).Queries("accessKey", "{accessKey:.*}")
+	apiRouter.Methods(http.MethodPost).Path("/change-password").HandlerFunc(iamApi.ChangePassword).Queries("newPassword", "{newPassword:.*}")
+	apiRouter.Methods(http.MethodPost).Path("/update-accessKey_status").HandlerFunc(iamApi.SetStatus).Queries("userName", "{userName:.*}", "status", "{status:.*}")
+	apiRouter.Methods(http.MethodGet).Path("/user-info").HandlerFunc(iamApi.GetUserInfo).Queries("userName", "{userName:.*}")
 
+	//sub user
+	apiRouter.Methods(http.MethodGet).Path("/list-users").HandlerFunc(iamApi.GetUserList)
 	apiRouter.Methods(http.MethodPost).Path("/put-user-policy").HandlerFunc(iamApi.PutUserPolicy).Queries("userName", "{userName:.*}", "policyName", "{policyName:.*}", "policyDocument", "{policyDocument:.*}")
 	apiRouter.Methods(http.MethodGet).Path("/get-user-policy").HandlerFunc(iamApi.GetUserPolicy).Queries("userName", "{userName:.*}", "policyName", "{policyName:.*}")
 	apiRouter.Methods(http.MethodGet).Path("/list-user-policy").HandlerFunc(iamApi.ListUserPolicies).Queries("userName", "{userName:.*}")
 	apiRouter.Methods(http.MethodPost).Path("/remove-user-policy").HandlerFunc(iamApi.DeleteUserPolicy).Queries("userName", "{userName:.*}", "policyName", "{policyName:.*}")
-	apiRouter.Methods(http.MethodPost).Path("/creat-policy").HandlerFunc(iamApi.CreatePolicy).Queries("policyName", "{policyName:.*}", "policyDocument", "{policyDocument:.*}")
-	apiRouter.Methods(http.MethodGet).Path("/user-info").HandlerFunc(iamApi.GetUserInfo).Queries("userName", "{userName:.*}")
-	apiRouter.Methods(http.MethodPost).Path("/change-password").HandlerFunc(iamApi.ChangePassword).Queries("newPassword", "{newPassword:.*}")
-	apiRouter.Methods(http.MethodPost).Path("/update-accessKey_status").HandlerFunc(iamApi.SetStatus).Queries("userName", "{userName:.*}", "status", "{status:.*}")
 
-	apiRouter.Methods(http.MethodPost).Path("/creat-group").HandlerFunc(iamApi.CreatGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
-	apiRouter.Methods(http.MethodGet).Path("/get_group").HandlerFunc(iamApi.GetGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
-	apiRouter.Methods(http.MethodPost).Path("/delete-group").HandlerFunc(iamApi.DeleteGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
-	apiRouter.Methods(http.MethodGet).Path("/list-groups").HandlerFunc(iamApi.ListGroups).Queries("pathPrefix", "{pathPrefix:.*}")
+	//apiRouter.Methods(http.MethodPost).Path("/creat-policy").HandlerFunc(iamApi.CreatePolicy).Queries("policyName", "{policyName:.*}", "policyDocument", "{policyDocument:.*}")
+
+	//apiRouter.Methods(http.MethodPost).Path("/creat-group").HandlerFunc(iamApi.CreatGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
+	//apiRouter.Methods(http.MethodGet).Path("/get_group").HandlerFunc(iamApi.GetGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
+	//apiRouter.Methods(http.MethodPost).Path("/delete-group").HandlerFunc(iamApi.DeleteGroup).Queries("groupName", "{groupName:.*}", "version", "{version:.*}")
+	//apiRouter.Methods(http.MethodGet).Path("/list-groups").HandlerFunc(iamApi.ListGroups).Queries("pathPrefix", "{pathPrefix:.*}")
 
 	apiRouter.NotFoundHandler = http.HandlerFunc(response.NotFoundHandler)
 }
