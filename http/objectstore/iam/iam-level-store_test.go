5b9bb6997941fd9c5bbe4470f87fafb67ac08e72
deanls <deanqs@qq.com>
2022-04-11 18:20:13 +0800

    test:annotate the TestLoadUser, add it later

--- http/objectstore/iam/iam-level-store_test.go
+++ http/objectstore/iam/iam-level-store_test.go
@@ -1,41 +1,34 @@
 package iam
 
-import (
-	"context"
-	"fmt"
-	"github.com/filedag-project/filedag-storage/http/objectstore/iam/auth"
-	"testing"
-)
-
-func TestLoadUser(t *testing.T) {
-	var iamSys IdentityAMSys
-	iamSys.Init()
-	//err := iamSys.saveUserIdentity(r.Context(), "test", UserIdentity{Credentials: auth.Credentials{
-	//	AccessKey:    "test",
-	//	SecretKey:    "test secret",
-	//	Expiration:   time.Now(),
-	//	SessionToken: "SessionToken",
-	//	Status:       "on",
-	//}})
-	//if err != nil {
-	//	return
-	//}
-
-	m := &auth.Credentials{}
-	err := iamSys.store.loadUser(context.Background(), "test", m)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//fmt.Println(m)
-	//a, err := iamSys.loadUsers(r.Context())
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//fmt.Println(a)
-	//err := iamSys.removeUserIdentity(r.Context(), "s")
-	//if err != nil {
-	//	return
-	//}
-}
+//func TestLoadUser(t *testing.T) {
+//	var iamSys IdentityAMSys
+//	iamSys.Init()
+//	//err := iamSys.saveUserIdentity(r.Context(), "test", UserIdentity{Credentials: auth.Credentials{
+//	//	AccessKey:    "test",
+//	//	SecretKey:    "test secret",
+//	//	Expiration:   time.Now(),
+//	//	SessionToken: "SessionToken",
+//	//	Status:       "on",
+//	//}})
+//	//if err != nil {
+//	//	return
+//	//}
+//
+//	m := &auth.Credentials{}
+//	err := iamSys.store.loadUser(context.Background(), "test", m)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	//fmt.Println(m)
+//	//a, err := iamSys.loadUsers(r.Context())
+//	//if err != nil {
+//	//	fmt.Println(err)
+//	//	return
+//	//}
+//	//fmt.Println(a)
+//	//err := iamSys.removeUserIdentity(r.Context(), "s")
+//	//if err != nil {
+//	//	return
+//	//}
+//}
