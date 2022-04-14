cbb200b82f480fffe7115b0615c47cddfc7b85b1
deanls <deanqs@qq.com>
2022-04-14 16:15:51 +0800

    test:test put object

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -14,11 +14,50 @@ const (
 )
 
 func TestS3ApiServer_PutObjectHandler(t *testing.T) {
-	u := "/testbucket/1.txt"
-	r1 := "123456"
-
-	req := testsign.MustNewSignedV4Request(http.MethodPut, u, int64(len(r1)), bytes.NewReader([]byte(r1)), "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
-	fmt.Println("put:", reqTest(req).Body.String())
+	bucketName := "/testbucket"
+	objectName := "/testobject"
+	r1 := "1234567"
+	// test cases with inputs and expected result for Bucket.
+	testCases := []struct {
+		bucketName string
+		objectName string
+		data       []byte
+		accessKey  string
+		secretKey  string
+		// expected output.
+		expectedRespStatus int // expected response status body.
+	}{
+		// Test case - 1.
+		// Fetching the entire Bucket and validating its contents.
+		{
+			bucketName:         bucketName,
+			objectName:         objectName,
+			data:               []byte(r1),
+			accessKey:          DefaultTestAccessKey,
+			secretKey:          DefaultTestSecretKey,
+			expectedRespStatus: http.StatusOK,
+		},
+		// Test case - 2.
+		// wrong accessKey.
+		{
+			bucketName:         bucketName,
+			objectName:         objectName,
+			accessKey:          "1",
+			secretKey:          "1",
+			expectedRespStatus: http.StatusForbidden,
+		},
+	}
+	reqPutBucket := testsign.MustNewSignedV4Request(http.MethodPut, bucketName, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+	fmt.Println("putbucket:", reqTest(reqPutBucket).Body.String())
+	// Iterating over the cases, fetching the object validating the response.
+	for i, testCase := range testCases {
+		req := testsign.MustNewSignedV4Request(http.MethodPut, testCase.bucketName+testCase.objectName, int64(len(r1)), bytes.NewReader(testCase.data), "s3", testCase.accessKey, testCase.secretKey, t)
+		result := reqTest(req)
+		if result.Code != testCase.expectedRespStatus {
+			t.Fatalf("Case %d: Expected the response status to be `%d`, but instead found `%d`", i+1, testCase.expectedRespStatus, result.Code)
+		}
+		fmt.Println("put:", reqTest(req).Body.String())
+	}
 
 }
 
