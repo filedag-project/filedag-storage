23a1cff199a5e586aee86e9c6ecc494e2ff51f02
deanls <deanqs@qq.com>
2022-04-15 11:16:51 +0800

    test:test headObjectHandler

--- http/objectstore/s3api/object_test.go
+++ http/objectstore/s3api/object_test.go
@@ -80,8 +80,7 @@ func TestS3ApiServer_PutObjectHandler(t *testing.T) {
 		},
 	}
 	reqPutBucket := testsign.MustNewSignedV4Request(http.MethodPut, bucketName, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
-	a := reqTest(reqPutBucket)
-	fmt.Println("putbucket:", a.Body.String())
+	fmt.Println("putbucket:", reqTest(reqPutBucket).Body.String())
 	// Iterating over the cases, fetching the object validating the response.
 	for i, testCase := range testCases {
 		req := testsign.MustNewSignedV4Request(http.MethodPut, testCase.bucketName+testCase.objectName, int64(len(r1)), bytes.NewReader(testCase.data), "s3", testCase.accessKey, testCase.secretKey, t)
@@ -155,6 +154,65 @@ func TestS3ApiServer_GetObjectHandler(t *testing.T) {
 	}
 
 }
+func TestS3ApiServer_HeadObjectHandler(t *testing.T) {
+	bucketName := "/testbucket"
+	objectName := "/testobject"
+
+	// test cases with inputs and expected result for Bucket.
+	testCases := []struct {
+		bucketName string
+		objectName string
+		data       []byte
+		header     http.Header
+		accessKey  string
+		secretKey  string
+		// expected output.
+		expectedRespStatus int // expected response status body.
+	}{
+		// Test case - 1.
+		// Fetching the entire Bucket and validating its contents.
+		{
+			bucketName:         bucketName,
+			objectName:         objectName,
+			accessKey:          DefaultTestAccessKey,
+			secretKey:          DefaultTestSecretKey,
+			expectedRespStatus: http.StatusOK,
+		},
+		// Test case - 2.
+		// wrong accessKey.
+		{
+			bucketName:         bucketName,
+			objectName:         objectName,
+			accessKey:          "1",
+			secretKey:          "1",
+			expectedRespStatus: http.StatusForbidden,
+		},
+		{
+			bucketName:         "/11",
+			objectName:         objectName,
+			accessKey:          DefaultTestAccessKey,
+			secretKey:          DefaultTestSecretKey,
+			expectedRespStatus: http.StatusNotFound,
+		},
+	}
+	reqPutBucket := testsign.MustNewSignedV4Request(http.MethodPut, bucketName, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+	fmt.Println("putbucket:", reqTest(reqPutBucket).Body.String())
+	r1 := "1234567"
+	reqputObject := testsign.MustNewSignedV4Request(http.MethodPut, bucketName+objectName, int64(len(r1)), bytes.NewReader([]byte(r1)), "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
+	// Add test case specific headers to the request.
+	reqTest(reqputObject)
+	// Iterating over the cases, fetching the object validating the response.
+	for i, testCase := range testCases {
+		req := testsign.MustNewSignedV4Request(http.MethodHead, testCase.bucketName+testCase.objectName, 0, nil, "s3", testCase.accessKey, testCase.secretKey, t)
+		// Add test case specific headers to the request.
+		result := reqTest(req)
+		if result.Code != testCase.expectedRespStatus {
+			t.Fatalf("Case %d: Expected the response status to be `%d`, but instead found `%d`", i+1, testCase.expectedRespStatus, result.Code)
+		}
+		fmt.Printf("Case %d: head:%v\n", i+1, result.Body.String())
+	}
+
+}
 func addCustomHeaders(req *http.Request, customHeaders http.Header) {
 	for k, values := range customHeaders {
 		for _, value := range values {
@@ -163,24 +221,6 @@ func addCustomHeaders(req *http.Request, customHeaders http.Header) {
 	}
 }
 
-//func TestS3ApiServer_GetObjectHandler(t *testing.T) {
-//	u := "http://127.0.0.1:9985/test/1.txt"
-//	req := testsign.MustNewSignedV4Request(http.MethodGet, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
-//
-//	//req.Header.Set("Content-Type", "text/plain")
-//	client := &http.Client{}
-//	res, err := client.Do(req)
-//	if err != nil {
-//		fmt.Println(err)
-//		return
-//	}
-//	defer res.Body.Close()
-//	body, err := ioutil.ReadAll(res.Body)
-//
-//	fmt.Println(res)
-//	fmt.Println(string(body))
-//
-//}
 //func TestS3ApiServer_CopyObjectHandler(t *testing.T) {
 //	u := "http://127.0.0.1:9985/test1/11.txt"
 //	req := testsign.MustNewSignedV4Request(http.MethodPut, u, 0, nil, "s3", DefaultTestAccessKey, DefaultTestSecretKey, t)
