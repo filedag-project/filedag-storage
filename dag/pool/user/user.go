c164202ab9a7870067134dc81b67a8fc7be1a0f9
hans <boda@kunyaokeji.com>
2022-04-28 21:30:21 +0800

    feat:add user identity interface

--- dag/pool/user/user.go
+++ dag/pool/user/user.go
@@ -1,6 +1,8 @@
 package user
 
 type dagPoolPolicy string
+type IdentityUser struct {
+}
 
 var (
 	OnlyRead  dagPoolPolicy = "only-read"
@@ -13,4 +15,25 @@ type DagPoolUser struct {
 	password string
 	policy   dagPoolPolicy
 	capacity uint64
+}
+
+// AddUser add user
+func (identity *IdentityUser) AddUser(user *DagPoolUser) error {
+	//Todo
+	var err error
+	return err
+}
+
+// RemoveUser remove user
+func (identity *IdentityUser) RemoveUser(userName string) error {
+	//Todo
+	var err error
+	return err
+}
+
+// QueryUser query user
+func (identity *IdentityUser) QueryUser(userName string) (*DagPoolUser, error) {
+	//Todo
+	var err error
+	return &DagPoolUser{}, err
 }
