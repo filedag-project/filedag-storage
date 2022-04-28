bfc6729303797a85e80e386fe6dcfbe18d27c077
deanls <deanqs@qq.com>
2022-04-28 16:28:35 +0800

    feat:define dag pool user

--- dag/pool/user/user.go
+++ dag/pool/user/user.go
@@ -1,1 +1,16 @@
 package user
+
+type dagPoolPolicy string
+
+var (
+	OnlyRead  dagPoolPolicy = "only-read"
+	OnlyWrite dagPoolPolicy = "only-write"
+	ReadWrite dagPoolPolicy = "readWrite"
+)
+
+type DagPoolUser struct {
+	username string
+	password string
+	policy   dagPoolPolicy
+	capacity uint64
+}
