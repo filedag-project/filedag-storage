ac3825fe77554475615b611179e027e1302c41b6
deanls <deanqs@qq.com>
2022-05-11 14:42:04 +0800

    fix:fix some GetNode err

--- dag/pool/pool.go
+++ dag/pool/pool.go
@@ -84,60 +84,6 @@ func GetPoolConfig() config.PoolConfig {
 	}
 }
 
-// CheckPolicy check user policy
-func (d *DagPool) CheckPolicy(ctx context.Context, policy userpolicy.DagPoolPolicy) bool {
-	s := strings.Split((ctx.Value("user")).(string), ",")
-	if len(s) != 2 {
-		return false
-	}
-	return d.Iam.CheckUserPolicy(s[0], s[1], policy)
-}
-
-// GetNode get the DagNode
-func (d *DagPool) GetNode(ctx context.Context, c cid.Cid) bserv.BlockService {
-	//todo mul node
-	get, err := d.TheNode.Get(c.String())
-	if err != nil {
-		return nil
-	}
-	return d.Blocks[get]
-}
-
-// UseNode get the DagNode
-func (d *DagPool) UseNode(ctx context.Context, c cid.Cid) bserv.BlockService {
-	//todo mul node
-	err := d.TheNode.Add(c.String(), 0)
-	if err != nil {
-		return nil
-	}
-	return d.Blocks[0]
-}
-
-// GetNodes get the DagNode
-func (d *DagPool) GetNodes(ctx context.Context, cids []cid.Cid) map[bserv.BlockService][]cid.Cid {
-	//todo mul node
-	//
-	m := make(map[bserv.BlockService][]cid.Cid)
-	for _, c := range cids {
-		get, err := d.TheNode.Get(c.String())
-		if err != nil {
-			return nil
-		}
-		m[d.Blocks[get]] = append(m[d.Blocks[get]], c)
-	}
-	return m
-}
-
-// UseNodes get the DagNode
-func (d *DagPool) UseNodes(ctx context.Context, c []cid.Cid) bserv.BlockService {
-	//todo mul node
-	err := d.TheNode.Add(c[0].String(), 0)
-	if err != nil {
-		return nil
-	}
-	return d.Blocks[0]
-}
-
 // Add adds a node to the DagPool, storing the block in the BlockService
 func (d *DagPool) Add(ctx context.Context, nd format.Node) error {
 	if !d.CheckPolicy(ctx, userpolicy.OnlyWrite) {
