bcf164cbe068396e3d85c3788e73ed110fb5057b
deanls <deanqs@qq.com>
2022-05-12 17:39:29 +0800

    fix:make PoolClient as a DAGService

--- http/objectstore/utils/dagpoolclient/client.go
+++ http/objectstore/utils/dagpoolclient/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"flag"
 	"github.com/filedag-project/filedag-storage/dag/pool/server"
+	"github.com/ipfs/go-cid"
+	format "github.com/ipfs/go-ipld-format"
 	logging "github.com/ipfs/go-log/v2"
 	"google.golang.org/grpc"
 	"time"
@@ -13,6 +15,43 @@ var log = logging.Logger("pool-client")
 var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
+
+type PoolClient struct {
+	pc server.DagPoolClient
+}
+
+func (p PoolClient) Get(ctx context.Context, cid cid.Cid) (format.Node, error) {
+	panic("implement me")
+}
+
+func (p PoolClient) GetMany(ctx context.Context, cids []cid.Cid) <-chan *format.NodeOption {
+	panic("implement me")
+}
+
+func (p PoolClient) Add(ctx context.Context, node format.Node) error {
+	_, err := p.pc.Add(ctx, &server.AddRequest{Block: node.RawData(), User: &server.PoolUser{
+		Username: "test",
+		Pass:     "test",
+	}})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p PoolClient) AddMany(ctx context.Context, nodes []format.Node) error {
+	panic("implement me")
+}
+
+func (p PoolClient) Remove(ctx context.Context, cid cid.Cid) error {
+	panic("implement me")
+}
+
+func (p PoolClient) RemoveMany(ctx context.Context, cids []cid.Cid) error {
+	panic("implement me")
+}
+
+var _ format.DAGService = &PoolClient{}
 
 func cli() {
 	flag.Parse()
