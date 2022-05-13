2fb2743d0ea7fc9aca39bf6bb7511d4a2a1e69bc
deanls <deanqs@qq.com>
2022-05-13 16:21:19 +0800

    fix:fix object connect pool

--- cmd/objectstore/server.go
+++ cmd/objectstore/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/filedag-project/filedag-storage/dag/pool"
 	"github.com/filedag-project/filedag-storage/http/objectstore/iamapi"
 	"github.com/filedag-project/filedag-storage/http/objectstore/s3api"
 	"github.com/filedag-project/filedag-storage/http/objectstore/store"
@@ -64,16 +63,9 @@ var startCmd = &cli.Command{
 			Value: defaultPort,
 		},
 		&cli.StringFlag{
-			Name:  "pool-db-path",
-			Usage: "set pool db path,if you need a local pool , use it",
-		},
-		&cli.StringFlag{
-			Name:  "pool-ip-path",
+			Name:  "pool-addr",
 			Usage: "set node path  if you need a local node , use it",
-		},
-		&cli.StringFlag{
-			Name:  "pool-batch-num",
-			Usage: "set pool batch num if you need a local pool , use it",
+			Value: "localhost:50001",
 		},
 		&cli.StringFlag{
 			Name:  "pool-user",
@@ -99,14 +91,8 @@ var startCmd = &cli.Command{
 				return err
 			}
 		}
-		if cctx.String("pool-db-path") != "" {
-			os.Setenv(pool.DagPoolLeveldbPath, cctx.String("pool-db-path"))
-		}
-		if cctx.String("pool-ip-path") != "" {
-			os.Setenv(pool.DagNodeIpOrPath, cctx.String("pool-ip-path"))
-		}
-		if cctx.String("pool-batch-num") != "" {
-			os.Setenv(pool.DagPoolImporterBatchNum, cctx.String("pool-batch-num"))
+		if cctx.String("pool-addr") != "" {
+			os.Setenv(store.PoolAddr, cctx.String("pool-addr"))
 		}
 		if cctx.String("pool-user") != "" {
 			os.Setenv(store.PoolUser, cctx.String("pool-user"))
