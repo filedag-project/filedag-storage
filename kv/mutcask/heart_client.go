cbb1f49ac7d5a60f187a7d32a25d9063aa550f1e
hans <hans@163.com>
2022-05-17 20:38:35 +0800

    feat: heart client send

--- kv/mutcask/heart_client.go
+++ kv/mutcask/heart_client.go
@@ -4,7 +4,6 @@ import (
 	beat "github.com/filedag-project/filedag-storage/dag/node/heart_beat"
 	"net"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -25,28 +24,14 @@ func sendHeartBeat(addr string) {
 	beat.Log("send over")
 }
 func sender(conn *net.TCPConn) {
-	for i := 0; i < 10; i++ {
-		words := strconv.Itoa(i) + " Hello I'm MyHeartbeat Client."
+	for {
+		words := "heart beat client."
 		msg, err := conn.Write([]byte(words))
 		if err != nil {
 			beat.Log(conn.RemoteAddr().String(), "Fatal error: ", err)
 			os.Exit(1)
 		}
 		beat.Log("sever accept", msg)
-		time.Sleep(2 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
-	for i := 0; i < 2; i++ {
-		time.Sleep(12 * time.Second)
-	}
-	for i := 0; i < 10; i++ {
-		words := strconv.Itoa(i) + " Hi I'm MyHeartbeat Client."
-		msg, err := conn.Write([]byte(words))
-		if err != nil {
-			beat.Log(conn.RemoteAddr().String(), "Fatal error: ", err)
-			os.Exit(1)
-		}
-		beat.Log("sever accept", msg)
-		time.Sleep(2 * time.Second)
-	}
-
 }
