2ac32990022a387a65bcf6e9f5a2a659a3fabfb3
hans <hans@163.com>
2022-05-19 17:54:50 +0800

    feat: data recovery test

--- dag/node/data_recovery_test.go
+++ dag/node/data_recovery_test.go
@@ -16,11 +16,11 @@ func TestRecovery_host(t *testing.T) {
 	}
 	err = json.Unmarshal(file, &nc)
 	dagNode, err := NewDagNode(nc)
-	//err = dagNode.recoveryHost("127.0.0.1", "127.0.0.1", "9010", "9013")
+	//err = dagNode.RepairDisk("127.0.0.1", "127.0.0.1", "9010", "9013")
 	//if err != nil {
 	//	fmt.Println(err)
 	//}
-	err = dagNode.recoveryDisk("127.0.0.1", "9013")
+	err = dagNode.RepairDisk("127.0.0.1", "9013")
 	if err != nil {
 		fmt.Println(err)
 	}
