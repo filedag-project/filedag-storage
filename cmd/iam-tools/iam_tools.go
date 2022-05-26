45cf8264b14e5d72c35915f1cff7938cc2d2d99e
deanls <deanqs@qq.com>
2022-05-26 18:56:24 +0800

    feat:add "iam-tools" example

--- cmd/iam-tools/iam_tools.go
+++ cmd/iam-tools/iam_tools.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
@@ -18,10 +17,14 @@ const (
 )
 
 var log = logging.Logger("tools")
-var addUserCmd = &cli.Command{
-	Name:  "adduser",
-	Usage: "add a user eg.demotools adduser --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg --pass=wpg123456",
+var cmd = &cli.Command{
+	Name:  "run",
+	Usage: "a user cmd eg.demotools --method --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg --pass=wpg123456",
 	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "method",
+			Usage: "the method",
+		},
 		&cli.StringFlag{
 			Name:  "addr",
 			Usage: "the addr of server eg.127.0.0.1:9985",
@@ -42,44 +45,33 @@ var addUserCmd = &cli.Command{
 			Name:  "pass",
 			Usage: "the password that you want add",
 		},
+		&cli.StringFlag{
+			Name:  "new-pass",
+			Usage: "the new pass that you want change",
+		},
+		&cli.StringFlag{
+			Name:  "status",
+			Usage: "the status that you want set",
+		},
 	},
 	Action: func(cctx *cli.Context) error {
-		var addr, accessKey, secretKey, username, pass string
-		if cctx.String("addr") != "" {
-			addr = cctx.String("addr")
-		} else {
-			log.Errorf("you must give the addr")
-			return xerrors.Errorf("you must give the addr")
+		var req *http.Request
+		var err error
+		switch cctx.String("method") {
+		case "adduser":
+			req, err = add(cctx)
+		case "deluser":
+			req, err = del(cctx)
+		case "getuser":
+			req, err = get(cctx)
+		case "change-pass":
+			req, err = cha(cctx)
+		case "set-status":
+			req, err = set(cctx)
+		default:
+			return xerrors.Errorf("method not allowed :%v", cctx.String("method"))
 		}
-		if cctx.String("access-key") != "" {
-			accessKey = cctx.String("access-key")
-		} else {
-			log.Errorf("you must give the access-key")
-			return xerrors.Errorf("you must give the access-key")
-		}
-		if cctx.String("secret-key") != "" {
-			secretKey = cctx.String("secret-key")
-		} else {
-			log.Errorf("you must give the secret-key")
-			return xerrors.Errorf("you must give the secret-key")
-		}
-		if cctx.String("username") != "" {
-			username = cctx.String("username")
-		} else {
-			log.Errorf("you must give the username")
-			return xerrors.Errorf("you must give the username")
-		}
-		if cctx.String("pass") != "" {
-			pass = cctx.String("pass")
-		} else {
-			log.Errorf("you must give the pass")
-			return xerrors.Errorf("you must give the pass")
-		}
-
-		req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+addUserUrl+"?accessKey="+username+"&secretKey="+pass,
-			0, nil, "s3", accessKey, secretKey)
-		if err != nil {
-			log.Errorf("mustNewSignedV4Request err: %v", err)
+		if err != nil || req == nil {
 			return err
 		}
 		client := http.DefaultClient
@@ -97,268 +89,174 @@ var addUserCmd = &cli.Command{
 		return nil
 	},
 }
-var delUserCmd = &cli.Command{
-	Name:  "deluser",
-	Usage: "del a user eg.demotools deluser --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "addr",
-			Usage: "the addr of server eg.127.0.0.1:9985",
-		},
-		&cli.StringFlag{
-			Name:  "access-key",
-			Usage: "the access-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "secret-key",
-			Usage: "the secret-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "username",
-			Usage: "the username that you want add",
-		},
-	},
-	Action: func(cctx *cli.Context) error {
-		var addr, accessKey, secretKey, username string
-		if cctx.String("addr") != "" {
-			addr = cctx.String("addr")
-		} else {
-			return xerrors.Errorf("you must give the addr")
-		}
-		if cctx.String("access-key") != "" {
-			accessKey = cctx.String("access-key")
-		} else {
-			return xerrors.Errorf("you must give the access-key")
-		}
-		if cctx.String("secret-key") != "" {
-			secretKey = cctx.String("secret-key")
-		} else {
-			return xerrors.Errorf("you must give the secret-key")
-		}
-		if cctx.String("username") != "" {
-			username = cctx.String("username")
-		} else {
-			return xerrors.Errorf("you must give the username")
-		}
-		req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+delUserUrl+"?accessKey="+username,
-			0, nil, "s3", accessKey, secretKey)
-		if err != nil {
-			return err
-		}
-		client := http.DefaultClient
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("response：%v\n", string(all))
-		return nil
-	},
-}
-var getUserCmd = &cli.Command{
-	Name:  "getuser",
-	Usage: "get a user info eg.demotools.exe getuser --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "addr",
-			Usage: "the addr of server eg.127.0.0.1:9985",
-		},
-		&cli.StringFlag{
-			Name:  "access-key",
-			Usage: "the access-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "secret-key",
-			Usage: "the secret-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "username",
-			Usage: "the username that you want get",
-		},
-	},
-	Action: func(cctx *cli.Context) error {
-		var addr, accessKey, secretKey, username string
-		if cctx.String("addr") != "" {
-			addr = cctx.String("addr")
-		} else {
-			return xerrors.Errorf("you must give the addr")
-		}
-		if cctx.String("access-key") != "" {
-			accessKey = cctx.String("access-key")
-		} else {
-			return xerrors.Errorf("you must give the access-key")
-		}
-		if cctx.String("secret-key") != "" {
-			secretKey = cctx.String("secret-key")
-		} else {
-			return xerrors.Errorf("you must give the secret-key")
-		}
-		if cctx.String("username") != "" {
-			username = cctx.String("username")
-		} else {
-			return xerrors.Errorf("you must give the username")
-		}
 
-		req, err := mustNewSignedV4Request(http.MethodGet, "http://"+addr+getUserUrl+"?accessKey="+username,
-			0, nil, "s3", accessKey, secretKey)
-		if err != nil {
-			return err
-		}
-		client := http.DefaultClient
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("response：%v\n", string(all))
-		return nil
-	},
-}
-var changePassCmd = &cli.Command{
-	Name:  "change-pass",
-	Usage: "change the user pass eg.demotools change-pass --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg --new-pass=wpg123456",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "addr",
-			Usage: "the addr of server eg.127.0.0.1:9985",
-		},
-		&cli.StringFlag{
-			Name:  "access-key",
-			Usage: "the access-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "secret-key",
-			Usage: "the secret-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "username",
-			Usage: "the username that you want get",
-		},
-		&cli.StringFlag{
-			Name:  "new-pass",
-			Usage: "the new pass that you want change",
-		},
-	},
-	Action: func(cctx *cli.Context) error {
-		var addr, accessKey, secretKey, username, pass string
-		if cctx.String("addr") != "" {
-			addr = cctx.String("addr")
-		} else {
-			return xerrors.Errorf("you must give the addr")
-		}
-		if cctx.String("access-key") != "" {
-			accessKey = cctx.String("access-key")
-		} else {
-			return xerrors.Errorf("you must give the access-key")
-		}
-		if cctx.String("secret-key") != "" {
-			secretKey = cctx.String("secret-key")
-		} else {
-			return xerrors.Errorf("you must give the secret-key")
-		}
-		if cctx.String("username") != "" {
-			username = cctx.String("username")
-		} else {
-			return xerrors.Errorf("you must give the username")
-		}
-		if cctx.String("new-pass") != "" {
-			pass = cctx.String("new-pass")
-		} else {
-			return xerrors.Errorf("you must give the new pass")
-		}
+func add(cctx *cli.Context) (*http.Request, error) {
+	var addr, accessKey, secretKey, username, pass string
+	if cctx.String("addr") != "" {
+		addr = cctx.String("addr")
+	} else {
+		log.Errorf("you must give the addr")
+		return nil, xerrors.Errorf("you must give the addr")
+	}
+	if cctx.String("access-key") != "" {
+		accessKey = cctx.String("access-key")
+	} else {
+		log.Errorf("you must give the access-key")
+		return nil, xerrors.Errorf("you must give the access-key")
+	}
+	if cctx.String("secret-key") != "" {
+		secretKey = cctx.String("secret-key")
+	} else {
+		log.Errorf("you must give the secret-key")
+		return nil, xerrors.Errorf("you must give the secret-key")
+	}
+	if cctx.String("username") != "" {
+		username = cctx.String("username")
+	} else {
+		log.Errorf("you must give the username")
+		return nil, xerrors.Errorf("you must give the username")
+	}
+	if cctx.String("pass") != "" {
+		pass = cctx.String("pass")
+	} else {
+		log.Errorf("you must give the pass")
+		return nil, xerrors.Errorf("you must give the pass")
+	}
 
-		req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+changePassUserUrl+"?newPassword="+pass+"&accessKey="+username,
-			0, nil, "s3", accessKey, secretKey)
-		if err != nil {
-			return err
-		}
-		client := http.DefaultClient
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("response：%v\n", string(all))
-		return nil
-	},
+	req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+addUserUrl+"?accessKey="+username+"&secretKey="+pass,
+		0, nil, "s3", accessKey, secretKey)
+	if err != nil {
+		log.Errorf("mustNewSignedV4Request err: %v", err)
+		return nil, err
+	}
+	return req, nil
 }
-var setStatusCmd = &cli.Command{
-	Name:  "set-status",
-	Usage: "set the user status",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "addr",
-			Usage: "the addr of server eg.127.0.0.1:9985",
-		},
-		&cli.StringFlag{
-			Name:  "access-key",
-			Usage: "the access-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "secret-key",
-			Usage: "the secret-key which have adduser policy",
-		},
-		&cli.StringFlag{
-			Name:  "username",
-			Usage: "the username that you want add",
-		},
-		&cli.StringFlag{
-			Name:  "status",
-			Usage: "the status that you want set",
-		},
-	},
-	Action: func(cctx *cli.Context) error {
-		var addr, accessKey, secretKey, username, status string
-		if cctx.String("addr") != "" {
-			addr = cctx.String("addr")
-		} else {
-			return xerrors.Errorf("you must give the addr")
-		}
-		if cctx.String("access-key") != "" {
-			accessKey = cctx.String("access-key")
-		} else {
-			return xerrors.Errorf("you must give the access-key")
-		}
-		if cctx.String("secret-key") != "" {
-			secretKey = cctx.String("secret-key")
-		} else {
-			return xerrors.Errorf("you must give the secret-key")
-		}
-		if cctx.String("username") != "" {
-			username = cctx.String("username")
-		} else {
-			return xerrors.Errorf("you must give the username")
-		}
-		if cctx.String("status") != "" {
-			status = cctx.String("status")
-		} else {
-			return xerrors.Errorf("you must give the status")
-		}
+func del(cctx *cli.Context) (*http.Request, error) {
+	var addr, accessKey, secretKey, username string
+	if cctx.String("addr") != "" {
+		addr = cctx.String("addr")
+	} else {
+		return nil, xerrors.Errorf("you must give the addr")
+	}
+	if cctx.String("access-key") != "" {
+		accessKey = cctx.String("access-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the access-key")
+	}
+	if cctx.String("secret-key") != "" {
+		secretKey = cctx.String("secret-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the secret-key")
+	}
+	if cctx.String("username") != "" {
+		username = cctx.String("username")
+	} else {
+		return nil, xerrors.Errorf("you must give the username")
+	}
+	req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+delUserUrl+"?accessKey="+username,
+		0, nil, "s3", accessKey, secretKey)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+func get(cctx *cli.Context) (*http.Request, error) {
+	var addr, accessKey, secretKey, username string
+	if cctx.String("addr") != "" {
+		addr = cctx.String("addr")
+	} else {
+		return nil, xerrors.Errorf("you must give the addr")
+	}
+	if cctx.String("access-key") != "" {
+		accessKey = cctx.String("access-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the access-key")
+	}
+	if cctx.String("secret-key") != "" {
+		secretKey = cctx.String("secret-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the secret-key")
+	}
+	if cctx.String("username") != "" {
+		username = cctx.String("username")
+	} else {
+		return nil, xerrors.Errorf("you must give the username")
+	}
 
-		req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+setStatusUrl+"?accessKey="+username+"&status="+status,
-			0, nil, "s3", accessKey, secretKey)
-		if err != nil {
-			return err
-		}
-		client := http.DefaultClient
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("response：%v\n", string(all))
-		return nil
-	},
+	req, err := mustNewSignedV4Request(http.MethodGet, "http://"+addr+getUserUrl+"?accessKey="+username,
+		0, nil, "s3", accessKey, secretKey)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+func cha(cctx *cli.Context) (*http.Request, error) {
+	var addr, accessKey, secretKey, username, pass string
+	if cctx.String("addr") != "" {
+		addr = cctx.String("addr")
+	} else {
+		return nil, xerrors.Errorf("you must give the addr")
+	}
+	if cctx.String("access-key") != "" {
+		accessKey = cctx.String("access-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the access-key")
+	}
+	if cctx.String("secret-key") != "" {
+		secretKey = cctx.String("secret-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the secret-key")
+	}
+	if cctx.String("username") != "" {
+		username = cctx.String("username")
+	} else {
+		return nil, xerrors.Errorf("you must give the username")
+	}
+	if cctx.String("new-pass") != "" {
+		pass = cctx.String("new-pass")
+	} else {
+		return nil, xerrors.Errorf("you must give the new pass")
+	}
+
+	req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+changePassUserUrl+"?newPassword="+pass+"&accessKey="+username,
+		0, nil, "s3", accessKey, secretKey)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+func set(cctx *cli.Context) (*http.Request, error) {
+	var addr, accessKey, secretKey, username, status string
+	if cctx.String("addr") != "" {
+		addr = cctx.String("addr")
+	} else {
+		return nil, xerrors.Errorf("you must give the addr")
+	}
+	if cctx.String("access-key") != "" {
+		accessKey = cctx.String("access-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the access-key")
+	}
+	if cctx.String("secret-key") != "" {
+		secretKey = cctx.String("secret-key")
+	} else {
+		return nil, xerrors.Errorf("you must give the secret-key")
+	}
+	if cctx.String("username") != "" {
+		username = cctx.String("username")
+	} else {
+		return nil, xerrors.Errorf("you must give the username")
+	}
+	if cctx.String("status") != "" {
+		status = cctx.String("status")
+	} else {
+		return nil, xerrors.Errorf("you must give the status")
+	}
+
+	req, err := mustNewSignedV4Request(http.MethodPost, "http://"+addr+setStatusUrl+"?accessKey="+username+"&status="+status,
+		0, nil, "s3", accessKey, secretKey)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
 }
