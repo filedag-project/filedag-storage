42ecf7c2ce3a9f291b630534b91057c51eb08b6b
deanls <deanqs@qq.com>
2022-05-27 11:24:09 +0800

    fix:fix example dagpool flag err

--- example/dagpool/main.go
+++ example/dagpool/main.go
@@ -13,25 +13,35 @@ import (
 	"google.golang.org/grpc"
 	"io/ioutil"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 )
 
-//go run -tags example main.go --pool-db-path=/tmp/leveldb2/pool.db --listen-addr=localhost:50001 --node-config-path=node_config.json --importer-batch-num=4
+//go run -tags example main.go run --pool-db-path=/tmp/leveldb2/pool.db --listen-addr=localhost:50001 --node-config-path=node_config.json --importer-batch-num=4
 func main() {
 	var leveldbPath, listenAddr, nodeConfigPath, importerBatchNum string
-	flag.StringVar(&leveldbPath, "pool-db-path", "", "set db path default:`/tmp/leveldb2/pool.db`")
-	flag.StringVar(&listenAddr, "listen-addr", "", "set listen addr default:`localhost:50001`")
-	flag.StringVar(&nodeConfigPath, "node-config-path", "", "set node config path,default:`dag/config/node_config.json'")
-	flag.StringVar(&importerBatchNum, "importer-batch-num", "", "set importer batch num default:4")
-	flag.Parse()
-	if leveldbPath == "" || listenAddr == "" || nodeConfigPath == "" || importerBatchNum == "" {
-		fmt.Printf("leveldbPath:%v, listenAddr:%v, nodeConfigPath:%v, importerBatchNum:%v", leveldbPath, listenAddr, nodeConfigPath, importerBatchNum)
-		fmt.Println("please check your input\n " +
-			"USAGE ERROR: go run -tags example main.go --pool-db-path= --listen-addr= --node-config-path= --importer-batch-num=")
+	f := flag.NewFlagSet("run", flag.ExitOnError)
+	f.StringVar(&leveldbPath, "pool-db-path", "", "set db path default:`/tmp/leveldb2/pool.db`")
+	f.StringVar(&listenAddr, "listen-addr", "", "set listen addr default:`localhost:50001`")
+	f.StringVar(&nodeConfigPath, "node-config-path", "", "set node config path,default:`dag/config/node_config.json'")
+	f.StringVar(&importerBatchNum, "importer-batch-num", "", "set importer batch num default:4")
+
+	switch os.Args[1] {
+	case "run":
+		f.Parse(os.Args[2:])
+		if leveldbPath == "" || listenAddr == "" || nodeConfigPath == "" || importerBatchNum == "" {
+			fmt.Printf("leveldbPath:%v, listenAddr:%v, nodeConfigPath:%v, importerBatchNum:%v", leveldbPath, listenAddr, nodeConfigPath, importerBatchNum)
+			fmt.Println("please check your input\n " +
+				"USAGE ERROR: go run -tags example main.go --pool-db-path= --listen-addr= --node-config-path= --importer-batch-num=")
+		}
+
+		run(leveldbPath, listenAddr, nodeConfigPath, importerBatchNum)
+	default:
+		fmt.Println("expected 'str' subcommands")
+		os.Exit(1)
 	}
 
-	run(leveldbPath, listenAddr, nodeConfigPath, importerBatchNum)
 }
 
 func run(leveldbPath, listenAddr, nodeConfigPath, importerBatchNum string) {
