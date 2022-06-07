afa5ad26ec6207c061f44c4394b079cbb0dda881
hans <hans@163.com>
2022-06-07 15:42:35 +0800

    fix:fix pin mock client

--- http/objectstore/utils/mock_client.go
+++ http/objectstore/utils/mock_client.go
@@ -19,13 +19,14 @@ func NewMockPoolClient(t *testing.T) (client.PoolClient, func()) {
 	return m, ctrl.Finish
 }
 
-func NewMockPinPoolClient(t *testing.T) (client.DataPin, func()) {
+func NewMockPinClient(t *testing.T) (client.DataPin, func()) {
 	ctrl := gomock.NewController(t)
 	m := mocks.NewMockDataPin(ctrl)
 	node := merkledag.NodeWithData([]byte("\b\u0002\u0012\a1234567\u0018\a"))
 	cid := node.Cid()
-	m.EXPECT().Pin(gomock.Any(), gomock.AssignableToTypeOf(node)).Return(nil).AnyTimes()
-	m.EXPECT().UnPin(gomock.Any(), gomock.AssignableToTypeOf(cid)).Return(node, nil).AnyTimes()
-	m.EXPECT().IsPin(gomock.Any(), gomock.AssignableToTypeOf(cid)).Return(nil).AnyTimes()
+	var is bool
+	m.EXPECT().Pin(gomock.Any(), gomock.AssignableToTypeOf(cid)).Return(nil).AnyTimes()
+	m.EXPECT().UnPin(gomock.Any(), gomock.AssignableToTypeOf(cid)).Return(nil).AnyTimes()
+	m.EXPECT().IsPin(gomock.Any(), gomock.AssignableToTypeOf(cid)).Return(is, nil).AnyTimes()
 	return m, ctrl.Finish
 }
