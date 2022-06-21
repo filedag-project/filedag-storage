765c690868d6477f33e58c74572f5030941248d4
deanls <deanqs@qq.com>
2022-06-21 11:33:11 +0800

    fix:fix example iam-tools  flag

--- example/iam-tools/adduser/main.go
+++ example/iam-tools/adduser/main.go
@@ -13,18 +13,18 @@ const (
 	addUserUrl = "/admin/v1/add-user"
 )
 
-//go run -tags example main.go adduser --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg --pass=wpg123456
+//go run -tags example main.go add-user --addr=127.0.0.1:9985 --access-key=test --secret-key=test --username=wpg --pass=wpg123456
 
 func main() {
 	var addr, accessKey, secretKey, username, pass string
-	f := flag.NewFlagSet("adduser", flag.ExitOnError)
+	f := flag.NewFlagSet("add-user", flag.ExitOnError)
 	f.StringVar(&addr, "addr", "", "the addr of server eg.127.0.0.1:9985")
 	f.StringVar(&accessKey, "access-key", "", "the access-key which have adduser policy")
 	f.StringVar(&secretKey, "secret-key", "", "the secret-key which have adduser policy")
 	f.StringVar(&username, "username", "", "the username that you want add")
 	f.StringVar(&pass, "pass", "", "the user password that you want add")
 	switch os.Args[1] {
-	case "adduser":
+	case "add-user":
 		f.Parse(os.Args[2:])
 		err := add(addr, accessKey, secretKey, username, pass)
 		if err != nil {
@@ -32,14 +32,14 @@ func main() {
 			return
 		}
 	default:
-		fmt.Println("expected 'adduser' subcommands")
+		fmt.Println("expected 'add-user' subcommands")
 		os.Exit(1)
 	}
 }
 func add(addr, accessKey, secretKey, username, pass string) error {
 	if addr == "" || accessKey == "" || secretKey == "" || username == "" || pass == "" {
 		fmt.Println("please check your input\n " +
-			"USAGE ERROR: go run -tags example main.go adduser --addr= --access-key= --secret-key= --username= --pass=")
+			"USAGE ERROR: go run -tags example main.go add-user --addr= --access-key= --secret-key= --username= --pass=")
 		return xerrors.Errorf("check your input")
 	}
 	err := exampleutils.SendSignedV4Request(http.MethodPost, "http://"+addr+addUserUrl+"?accessKey="+username+"&secretKey="+pass,
