c16af474c0e33e1d9336bac6ba1df4334ba5eef4
deanls <deanqs@qq.com>
2024-01-24 17:15:50 +0800

    test: test script print info

--- testscript/cluster.sh
+++ testscript/cluster.sh
@@ -1,75 +1,100 @@
-function init() {
-    nohup ./datanode daemon --listen=127.0.0.1:9011 --datadir=./dn-data1 >dn1log.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9012 --datadir=./dn-data2 >dn2log.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9013 --datadir=./dn-data3 >dn3log.log 2>&1 &
+#!/bin/bash
 
-    nohup ./datanode daemon --listen=127.0.0.1:9014 --datadir=./dn-data4 > dn4.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9015 --datadir=./dn-data5 > dn5.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9016 --datadir=./dn-data6 > dn6.log 2>&1 &
-
-    nohup ./datanode daemon --listen=127.0.0.1:9017 --datadir=./dn-data7 > dn7.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9018 --datadir=./dn-data8 > dn8.log 2>&1 &
-    nohup ./datanode daemon --listen=127.0.0.1:9019 --datadir=./dn-data9 > dn9.log 2>&1 &
-    echo "start datanode success"
-    sleep 3
-    nohup ./dagpool daemon --datadir=./dp-data > dp.log 2>&1 &
-    echo "start dagpool success"
-    sleep 3
-    nohup ./objectstore daemon --pool-addr=127.0.0.1:50001 --pool-user=dagpool --pool-password=dagpool --datadir=./store-data >obj.log 2>&1 &
-    echo "start objectstore success"
-    sleep 3
-  ./dagpool cluster add conf/node_config.json conf/node_config2.json conf/node_config3.json
-   echo "add  datanode to dagpool cluster success"
+function green_echo() {
+    local green=$(tput setaf 2)
+    local reset=$(tput sgr0)
+    echo "${green}$1${reset}"
 }
 
-#health report of storage nodes and status
-function statusAndBalance() {
+function start_datanode() {
+    local start_port=$1
+    local end_port=$2
+    local datadir_prefix=$3
+
+    for port in $(seq $start_port $end_port); do
+        nohup ./datanode daemon --listen=127.0.0.1:$port --datadir=./$datadir_prefix-data$port > dn$port.log 2>&1 &
+    done
+    green_echo "Start datanodes success"
+    sleep 3
+}
+
+function start_dagpool() {
+    nohup ./dagpool daemon --datadir=./dp-data > dp.log 2>&1 &
+    green_echo "Start dagpool success"
+    sleep 3
+}
+
+function start_objectstore() {
+    nohup ./objectstore daemon --pool-addr=127.0.0.1:50001 --pool-user=dagpool --pool-password=dagpool --datadir=./store-data > obj.log 2>&1 &
+    green_echo "Start objectstore success"
+    sleep 3
+}
+
+function add_datanodes_to_dagpool() {
+    ./dagpool cluster add conf/node_config.json conf/node_config2.json conf/node_config3.json > /dev/null 2>&1
+    green_echo "Add datanodes to dagpool cluster success"
+}
+
+function status_and_balance() {
     ./dagpool cluster status
+    echo ""
     echo "*****Balance*****"
     ./dagpool cluster balance
-    ./dagpool cluster status
+    result
 }
 
-#expansion of storage nodes
 function expansion() {
-   echo "2.expansion"
-   nohup ./datanode daemon --listen=127.0.0.1:9020 --datadir=./dn-data10 > dn10.log 2>&1 &
-   nohup ./datanode daemon --listen=127.0.0.1:9021 --datadir=./dn-data11 > dn11.log 2>&1 &
-   nohup ./datanode daemon --listen=127.0.0.1:9022 --datadir=./dn-data12 > dn12.log 2>&1 &
-   ./dagpool cluster add conf/node_config4.json
-   ./dagpool cluster status
+    echo ""
+    echo "*****Expansion*****"
+    start_datanode 9020 9022 dn
+    ./dagpool cluster add conf/node_config4.json > /dev/null 2>&1
+    result
 }
-#scaling of storage nodes
+
 function scaling() {
-  echo "3.scaling"
-   ./dagpool cluster migrate dag_node3 dag_node2 10923-16383
-   sleep 3
-   ./dagpool cluster remove dag_node3
-   ./dagpool cluster status
+    echo ""
+    echo "*****Scaling*****"
+    ./dagpool cluster migrate dag_node3 dag_node2 10923-16383 > /dev/null 2>&1
+    sleep 3
+    ./dagpool cluster remove dag_node3 > /dev/null 2>&1
+    result
 }
+
 function clean() {
-    rm -rf ./dn-data*
-    rm -rf ./dp-data*
-    rm -f ./dn*.log
-    rm -rf ./dp-db
-    rm -f ./dp.log
-    rm -rf ./store-data
-    rm -f ./obj.log
+    rm -rf ./dn-data* ./dp-data* ./dp-db ./store-data
+    rm -f ./dn*.log ./dp.log ./obj.log
     ps -ef | grep datanode | awk '{print $2}' | xargs kill
     ps -ef | grep dagpool | awk '{print $2}' | xargs kill
-
     ps -ef | grep objectstore | awk '{print $2}' | xargs kill
-    }
-# 根据参数选择执行对应的函数
+}
+
+function result() {
+    local command_output
+    command_output=$(./dagpool cluster status 2>&1)
+    echo "$(green_echo "$command_output")"
+}
+
 case "$1" in
     "init")
-        init
+        start_datanode 9011 9019 dn
+        start_dagpool
+        start_objectstore
+        add_datanodes_to_dagpool
         ;;
-      "clean")
+    "all")
+        start_datanode 9011 9019 dn
+        start_dagpool
+        start_objectstore
+        add_datanodes_to_dagpool
+        status_and_balance
+        expansion
+        scaling
+        ;;
+    "clean")
         clean
         ;;
     "statusAndBalance")
-        statusAndBalance
+        status_and_balance
         ;;
     "expansion")
         expansion
@@ -78,7 +103,7 @@ case "$1" in
         scaling
         ;;
     *)
-        echo "Invalid argument. Usage: $0 {init|statusAndBalance|expansion|scaling}"
+        echo "Invalid argument. Usage: $0 {init|all|clean|statusAndBalance|expansion|scaling}"
         exit 1
         ;;
 esac
